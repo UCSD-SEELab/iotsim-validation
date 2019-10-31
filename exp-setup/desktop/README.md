c09679dd42d3952bbffdd7270408e9deaeedcbcd
Orienfish <x1yu@ucsd.edu>
2019-10-30 21:18:38 -0700

    copy rpi3b to desktop

---
+++ exp-setup/desktop/README.md
@@ -0,0 +1,126 @@
+# Setup MQTT, Scikit-Learn, Keras on Linux Desktop
+
+Almost directly copied from `rpi3b/README.md`.
+
+[Environment Setup](#Environment-Setup)
+
+[Run Instructions](#Run-Instructions)
+
+## Environment Setup
+
+### MQTT Broker and Client
+
+1. Install `paho-mqtt`:
+
+   ```shell
+   sudo apt install python3-pip
+   sudo pip3 install paho-mqtt
+   ```
+
+2. Install mosquitto server:
+
+   ```shell
+   sudo apt update
+   sudo apt install -y mosquitto mosquitto-clients
+   
+   # to make mosquitto auto start on boot up
+   sudo systemctl enable mosquitto.service
+   ```
+
+3. To use mosquitto:
+
+   To start the broker, use the following command to let the broker start listen on port <port> with verbose:
+
+   ```shell
+   mosquitto -p <port> -v
+   ```
+
+   On the client side, to publish a topc, use [mosquitto_pub](https://mosquitto.org/man/mosquitto_pub-1.html).
+
+   ```shell
+   mosquitto_pub -h <hostname> -p <port> -u <username> -P <password> \ 
+   -t topic -i <client_id> -k <keepalive> -q <qos> \
+   { -f file | -l | -m message | -n | -s }
+   ```
+
+   To subscribe a topic, use [mosquitto_sub](https://mosquitto.org/man/mosquitto_sub-1.html).
+   
+   ```shell
+   mosquitto_sub -h <hostname> -p <port> -u <username> -P <password> \
+   -t topic -i <client_id> -k <keepalive> -q <qos>
+   ```
+   
+
+### Scikit-Learn
+
+1. Installation on Raspberry Pi.
+
+   ```shell
+   pip3 install scikit-learn
+   ```
+
+2. Other required modules: `matplotlib` and `numpy`:
+
+   ```shell
+   pip3 install matplotlib
+   pip3 install numpy
+   ```
+
+### Keras (Optional)
+
+Although we end up with using `scikit-learn` instead of Keras, the installation and a simple example of Keras is noted here.
+
+1. Install Keras on Raspberry Pi. Follow the tutorial [here](https://medium.com/@abhizcc/installing-latest-tensor-flow-and-keras-on-raspberry-pi-aac7dbf95f2) to install tensorflow and Keras.
+   Note you can download the latest tensorflow compatible with the Python version, from the [release page](https://github.com/lhelontra/tensorflow-on-arm/releases/) provided by [lhelontra](https://github.com/lhelontra). In this case, *tensorflow-1.14.0-cp35-none-linux_armv7l.whl* for  Raspberry pi 2/3 is selected. 
+
+2. A simple example of Keras on Raspberry Pi is the MNIST classification. Run the script in this folder to test:
+
+   ```shell
+   python3 keras_mnist.py
+   ```
+
+## Preparation
+
+You need to train the MLP model for every input-hidden layer-output (fix to 1) combination.
+
+To train a model with (5,2) hidden layer on `output.jpg`:
+
+```shell
+python3 mlp.py -m train -l 5 2 -p output.jpg
+```
+
+To infer an image with the model obtained in the above training:
+
+```shell
+python3 mlp.py -m infer -l 5 2 -p output.jpg
+```
+
+## Run Instructions
+
+1. Start the mosquitto broker
+
+   ```shell
+   mosquitto -p 61613 -v
+   ```
+
+2. Start the receiver client on Raspberry Pi 3B, specifying whether perform local processing:
+
+   Do local image processing with a MLP with hidden layers (5,10), run Linear Regression of 18 inputs and 10 outputs at the same time:
+
+   ```shell
+   python3 mqtt.py -ip 1 -il 5 10 -pp 1 -pl 18 10
+   ```
+
+   Do not process locally. Instead, directly forward the image:
+
+   ```shell
+   python3 mqtt.py
+   ```
+
+3. Trigger the image publishing on Raspberry Pi zero. The image is set to a resolution of 1024*1024, with an interval of 0.5s.
+
+   ```shell
+   python3 main.py 0.5 1024 1024 # in ./rpi0
+   ```
+
+   
---
+++ exp-setup/desktop/mqtt.py
@@ -0,0 +1,161 @@
+#!/usr/bin/python3
+
+'''
+Example module to receive data on the desktop
+Work on Python 3.7.4 of Manjaro Linux
+
+To run without processing:
+    python3 mqtt.py
+To run with image processing, hidden layer (5, 10),
+Linear Regression on photon data, 18 inputs and 10 outputs:
+    python3 -ip 1 -il 5 10 -pp 1 -pl 18 10
+Author: Xiaofan Yu
+Date: 10/29/2019
+'''
+import paho.mqtt.client as mqtt
+import paho.mqtt.publish as publish
+import argparse
+from mlp import infer
+from lr import LinearRegression
+MQTT_SERVER = '192.168.1.57'
+MQTT_PORT = 61613
+
+def img_process(payload, *args):
+    # process img locally with specified hidden layers
+    # write down the image
+    hidden_layer = args
+    img_path = './output.jpg'
+    with open(img_path, 'wb') as f:
+        f.write(payload)
+        f.close()
+
+    # process the image and publish the result
+    try:
+        payload = infer(img_path, hidden_layer)
+    except Exception as e:
+        print(e)
+    payload = bytearray(payload)
+    return payload
+
+def photon_process(payload, *args):
+    payload = payload.decode('utf-8')
+    payload = payload.split('[[')
+    header = payload[0] # time stamp information
+    in_data = payload[1].split(']]')[0]
+    in_data = in_data.split(',')
+    in_data = [float(d) for d in in_data] # extract the data in float
+
+    assert(photon_process.lr.in_size == len(in_data))
+
+    out_data = photon_process.lr.run(in_data)
+    payload = header + str(out_data) + '}'
+    payload = payload.encode('utf-8')
+
+    return payload
+
+
+class routine():
+    def __init__(self, sub_topic: str, pub_topic: str, \
+                process: bool, func: callable):
+        self.sub_topic = sub_topic
+        self.pub_topic = pub_topic
+        self.process = process
+        self.func = func
+        self.args = None
+
+img_routine = routine('rpi3b/img', None, False, img_process)
+photon_routine = routine('rpi3b/photon', 'None', False, photon_process)
+receive_routine = [img_routine, photon_routine]
+
+class myMQTTReceiver():
+    def __init__(self, server_ip=None, server_port=None, client_id='desktop'):
+        '''
+        Args:
+            server_ip, server_port: ip and port of broker
+            client_id: client id of this rpi 3b
+            sub_topic: the list of topic that this device subscribes
+            process: whether processing the image before publish
+            hidden_layer: the number of units in each hidden layer of
+                MLP, if process the image
+        '''
+        self.hostname = server_ip
+        self.port = server_port
+        self.client = mqtt.Client(client_id=client_id)
+        self.client.on_connect = self.on_connect
+        self.client.on_message = self.on_message
+        self.client.connect(host=self.hostname, port=self.port)
+        self.qos = 0
+
+
+    def on_connect(self, client, userdata, flags, rc):
+        # The callback for when the client receives a CONNACK response
+        # from the server.
+        print("Connected with result code "+str(rc))
+
+        # Subscribing in on_connect() means that if we lose the
+        # connection and reconnect then subscriptions will be renewed.
+        for r in receive_routine:
+            self.client.subscribe(r.sub_topic)
+            print("Subscribe topic {}".format(r.sub_topic))
+
+
+    def on_message(self, client, userdata, msg):
+        # process for different topics
+        for r in receive_routine:
+            if msg.topic == r.sub_topic:
+                print('Message of topic {} received'.format(r.sub_topic))
+                if not r.process:
+                    # if no processing here, directly save the data
+                    pass
+                else:
+                    # perform local computation
+                    payload = r.func(msg.payload, *r.args)
+                    pass
+
+                return
+
+
+def main():
+    # parse the parameters
+    parser = argparse.ArgumentParser(description='Run MQTT client to receive \
+            data and process it locally if specified')
+    parser.add_argument('-ip', '--imageprocess', type=bool, \
+            dest='imgprocess', \
+            default=False, \
+            help='Whether run local image processing')
+    parser.add_argument('-il', '--imagelayer', type=int, nargs='+', \
+            dest='imglayer', \
+            default=None, \
+            help='Specify the number of nodes in each hidden layer \
+                for MLP used in image processing')
+    parser.add_argument('-pp', '--photonprocess', type=bool, \
+            dest='photonprocess', \
+            default=False, \
+            help='Whether run local photon data processing')
+    parser.add_argument('-pl', '--photonlayer', type=int, nargs='+', \
+            dest='photonlayer', \
+            default=None, \
+            help='Specify the number of inputs and outputs \
+                for Linear Regression used in photon data processing')
+
+    args = parser.parse_args()
+    print(args.imgprocess, args.imglayer, args.photonprocess, args.photonlayer)
+    try:
+        img_routine.process = args.imgprocess
+        img_routine.args = tuple(args.imglayer)
+        photon_routine.process = args.photonprocess
+        photon_routine.args = tuple(args.photonlayer)
+    except:
+        pass
+
+    # set LinearRegression
+    if photon_routine.process:
+        photon_process.lr = LinearRegression(photon_routine.args[0], \
+            photon_routine.args[1])
+
+    # start MQTT client
+    mqttRev = myMQTTReceiver(server_ip=MQTT_SERVER, server_port=MQTT_PORT)
+    mqttRev.client.loop_forever()
+
+if __name__ == '__main__':
+    main()
