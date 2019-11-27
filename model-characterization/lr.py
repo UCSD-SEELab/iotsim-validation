fix the size calculation bug

model-characterization/lr.py
@@ -58,9 +58,9 @@ if __name__ == '__main__':
 
     args = parser.parse_args()
 
-    # convert KByte to number of floats, << 3 then >> 2
-    in_size = args.input << 1
-    out_size = args.output << 1
+    # convert KByte to number of floats, << 10 then >> 2
+    in_size = args.input << 8
+    out_size = args.output << 8
     lr = LinearRegression(in_size, out_size)
     a = np.random.normal(size=(1, in_size))
 